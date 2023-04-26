f128e3a5e1c8cb4334b6d95355256f951642f825
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-04-26 19:19:44 +1000

    Refactor EnchantBundle

--- Paper/src/main/kotlin/me/partlysunny/paper/util/classes/EnchantBundle.kt
+++ Paper/src/main/kotlin/me/partlysunny/paper/util/classes/EnchantBundle.kt
@@ -6,46 +6,46 @@ import org.bukkit.enchantments.Enchantment
 enum class EnchantBundle(private val bundle: HashMap<Enchantment, Int>) {
     BLANK(HashMap<Enchantment, Int>()),
     DURABILITY(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.DURABILITY, 3).put(Enchantment.MENDING, 1).build()
     ),
     ARMOR_HELM(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.PROTECTION_ENVIRONMENTAL, 4).put(Enchantment.DURABILITY, 3)
             .put(Enchantment.WATER_WORKER, 1).put(Enchantment.OXYGEN, 3).put(Enchantment.MENDING, 1).build()
     ),
     ARMOR_CHESTPLATE(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.PROTECTION_ENVIRONMENTAL, 4).put(Enchantment.DURABILITY, 3).put(Enchantment.MENDING, 1)
             .build()
     ),
     ARMOR_LEGGINGS(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.PROTECTION_ENVIRONMENTAL, 4).put(Enchantment.DURABILITY, 3).put(Enchantment.MENDING, 1)
             .build()
     ),
     ARMOR_BOOTS(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.PROTECTION_ENVIRONMENTAL, 4).put(Enchantment.DURABILITY, 3)
             .put(Enchantment.PROTECTION_FALL, 1).put(Enchantment.DEPTH_STRIDER, 3).put(Enchantment.MENDING, 1).build()
     ),
     WEAPON_FIRE(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.DAMAGE_ALL, 4).put(Enchantment.DURABILITY, 3).put(Enchantment.FIRE_ASPECT, 2)
             .put(Enchantment.SWEEPING_EDGE, 3).put(Enchantment.LOOT_BONUS_MOBS, 3).put(Enchantment.MENDING, 1).build()
     ),
     WEAPON(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.DAMAGE_ALL, 4).put(Enchantment.DURABILITY, 3).put(Enchantment.SWEEPING_EDGE, 3)
             .put(Enchantment.LOOT_BONUS_MOBS, 3).put(Enchantment.MENDING, 1).build()
     ),
     PICKAXE_FORTUNE(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.DIG_SPEED, 5).put(Enchantment.DURABILITY, 3).put(Enchantment.LOOT_BONUS_BLOCKS, 3)
             .put(Enchantment.MENDING, 1).build()
     ),
     PICKAXE_SILK(
-        HashMapBuilder.builder<Enchantment, Int>(Enchantment::class.java, Int::class.java)
+        HashMapBuilder.builder(Enchantment::class.java, Int::class.java)
             .put(Enchantment.DIG_SPEED, 5).put(Enchantment.DURABILITY, 3).put(Enchantment.SILK_TOUCH, 3)
             .put(Enchantment.MENDING, 1).build()
     );
