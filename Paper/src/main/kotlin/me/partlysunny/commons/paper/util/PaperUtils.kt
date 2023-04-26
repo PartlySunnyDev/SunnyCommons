e9119344cb7c5168bd0ab3c512a76abd2a87c086
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-04-26 19:44:14 +1000

    Fixed warnings and deprecation

--- Paper/src/main/kotlin/me/partlysunny/commons/paper/util/PaperUtils.kt
+++ Paper/src/main/kotlin/me/partlysunny/commons/paper/util/PaperUtils.kt
@@ -1,15 +1,15 @@
 package me.partlysunny.commons.paper.util
 
 import me.partlysunny.commons.paper.SunnyUtilsCore
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
-import org.bukkit.ChatColor
 import org.bukkit.Sound
 import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.potion.PotionEffectType
 import org.bukkit.potion.PotionType
-import java.util.regex.Pattern
+import org.bukkit.util.ChatPaginator
 
 object PaperUtils {
 
@@ -20,12 +20,14 @@ object PaperUtils {
         scheduler.runTaskLater(p, Runnable { t.cancel() }, stopAfter)
     }
 
+    @Suppress("UNCHECKED_CAST")
     fun <T> getOrDefault(y: ConfigurationSection, key: String?, def: T): T? {
         return if (y.contains(key!!)) {
             y[key] as T?
         } else def
     }
 
+    @Suppress("UNCHECKED_CAST")
     fun <T> getOrError(y: ConfigurationSection, key: String): T? {
         if (y.contains(key)) {
             return y[key] as T?
@@ -36,69 +38,17 @@ object PaperUtils {
     @JvmOverloads
     fun splitLoreForLine(
         input: String,
-        linePrefix: String = ChatColor.GRAY.toString(),
+        linePrefix: String = "<gray>",
         lineSuffix: String = "",
         width: Int = 30
     ): List<String> {
-        val array = input.toCharArray()
-        val out: MutableList<String> = ArrayList()
-        var currentColor = ""
-        var cachedColor = ""
-        var wasColorChar = false
-        var currentLine = StringBuilder(linePrefix)
-        var currentWord = StringBuilder()
-        for (i in array.indices) {
-            val c = array[i]
-            if (wasColorChar) {
-                wasColorChar = false
-                cachedColor = currentColor
-                val pattern = Pattern.compile("[0-9a-fkmolnr]")
-                if (pattern.matcher(c.toString() + "").matches()) {
-                    if (c == 'r') {
-                        currentColor = ChatColor.COLOR_CHAR.toString() + "r"
-                    } else {
-                        currentColor += ChatColor.COLOR_CHAR.toString() + "" + c
-                    }
-                }
-                currentWord.append(ChatColor.COLOR_CHAR.toString() + "").append(c)
-                continue
-            }
-            if (c == '\n') {
-                currentLine.append(currentWord)
-                currentWord = StringBuilder()
-                out.add(currentLine.toString() + lineSuffix)
-                currentLine = StringBuilder(linePrefix + cachedColor + currentWord)
-                cachedColor = currentColor
-                continue
-            }
-            if (c == ' ') {
-                if ((currentLine.toString() + currentWord.toString()).replace(
-                        "ยง[0-9a-fklmnor]".toRegex(),
-                        ""
-                    ).length > width
-                ) {
-                    out.add(currentLine.toString() + lineSuffix)
-                    currentLine = StringBuilder("$linePrefix$cachedColor$currentWord ")
-                } else {
-                    currentLine.append(currentWord).append(" ")
-                }
-                cachedColor = currentColor
-                currentWord = StringBuilder()
-                continue
-            }
-            if (c == ChatColor.COLOR_CHAR) {
-                wasColorChar = true
-                continue
-            }
-            currentWord.append(c)
-        }
-        currentLine.append(currentWord)
-        out.add(currentLine.toString() + lineSuffix)
-        return out
+        val wordWrap = ChatPaginator.wordWrap(input, width).toList()
+        wordWrap.map { linePrefix + it + lineSuffix }
+        return wordWrap
     }
 
     fun invalid(message: String, p: Player) {
-        p.sendMessage(ChatColor.RED.toString() + message)
+        p.sendMessage(MiniMessage.miniMessage().deserialize("<red>$message"))
         p.playSound(p.location, Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f)
     }
 
